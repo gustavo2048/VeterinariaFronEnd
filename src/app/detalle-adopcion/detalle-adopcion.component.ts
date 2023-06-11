9263f70c24c1dd314d4606bfb90fc0c06af02838
rck <marcoslevis.rck@gmail.com>
2023-06-11 20:23:04 -0300

    asd

--- src/app/detalle-adopcion/detalle-adopcion.component.ts
+++ src/app/detalle-adopcion/detalle-adopcion.component.ts
@@ -8,6 +8,7 @@ import { AdopcionService } from '../service/adopcion.service';
 import { ContactoComponent } from '../contacto/contacto.component';
 import { VeterinariaService } from '../service/veterinaria.service';
 import { Mascota } from '../modelo/Mascota';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-detalle-adopcion',
@@ -40,7 +41,7 @@ export class DetalleAdopcionComponent {
 
 
   constructor( private _snackBar: MatSnackBar, public dialog: MatDialog, public dialogRef: MatDialogRef<DetalleAdopcionComponent>,
-     @Inject(MAT_DIALOG_DATA) public data: Adopcion,  private adopcionService: AdopcionService, private veterinariaService: VeterinariaService) {
+     @Inject(MAT_DIALOG_DATA) public data: Adopcion,  private adopcionService: AdopcionService, private veterinariaService: VeterinariaService, private authService: AuthService) {
       
 
       this.adopcion = data;
@@ -146,6 +147,11 @@ export class DetalleAdopcionComponent {
           this.desHabilitar();
       }
     }
+
+    mostrar(roles: string[]){
+      return ( roles.includes(this.authService.usertype()));
+    }
+  
     cancelarEdicion(){
       this.tit = new FormControl({value: this.adopcion.titulo , disabled: true},[Validators.required]);
       this.desc = new FormControl({value: this.adopcion.descripcion , disabled: true},[Validators.required]);
